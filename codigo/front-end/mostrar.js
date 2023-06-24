e54ce817688864a8add2ab1bedd2440d057f8f41
vicho-bece <80727735+vicho-bece@users.noreply.github.com>
2023-06-23 22:06:55 -0400

    Ajuste en el boton de agregar productos

--- codigo/front-end/mostrar.js
+++ codigo/front-end/mostrar.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
         for(let i = 0; data.result[i]!=null; i++){
             var carta = `<br><div class="card"><div class="card-body"><h5 class="card-title">`+data.result[i].nombreProducto+`</h5>
             <p class="card-text">Codigo del Producto: `+data.result[i].Codigo+`</p><p class="card-text">Cantidad de stock: `+data.result[i].stock+`</p>
-            <p class="card-text">Fabricante: `+data.result[i].fabricante+`</p><a href="#" class="btn btn-primary">Agregar al carrito</a>`
+            <p class="card-text">Fabricante: `+data.result[i].fabricante+`</p><p class="Precio">$100</p><a href="#" class="btn btn-primary btn-add">Agregar al carrito</a>`
             $("#productos").append(carta);
         }
     })
