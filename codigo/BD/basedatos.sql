34bddca96646d5c64a796ea741ea30f7ef862b29
vicho-bece <80727735+vicho-bece@users.noreply.github.com>
2023-06-30 20:18:39 -0400

    Archivo.sql

--- codigo/BD/basedatos.sql
+++ codigo/BD/basedatos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-06-2023 a las 19:47:03
+-- Tiempo de generación: 01-07-2023 a las 02:17:45
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
